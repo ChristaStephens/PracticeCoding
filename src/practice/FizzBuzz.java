93e6bd098100edf694ab43886484343e8b0c0d3c
Christa Stephens <ChristaStephens@outlook.com>
2019-01-20 22:21:14 -0500

    if else for fizz buzz

--- src/practice/FizzBuzz.java
+++ src/practice/FizzBuzz.java
@@ -4,9 +4,23 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		// Doing fizz buzz. for multiples of 3 write fizz
-		//for multiples of 5 write buzz. for multiples of 3 
-		//and 5 write fizzbuzz
-	
+		// for multiples of 5 write buzz. for multiples of 3
+		// and 5 write fizzbuzz
+
+		for (int i = 100; i < 1; i++) {
+
+			if (i % 15 == 0) {
+				System.out.println("fizzbuzz");
+			}
+			else if (i % 5 ==0) {
+				System.out.println("buzz");
+			}
+			else if(i % 3 ==0) {System.out.println("fizz");
+			}
+			else {System.out.println(i);
+			}
+			
+		}
 
 	}
 
