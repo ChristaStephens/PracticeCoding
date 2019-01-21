a1aad70d98e86f0e576b7e0c33292473f1a3a243
Christa Stephens <ChristaStephens@outlook.com>
2019-01-21 16:41:28 -0500

    completed reverse string

--- src/practice/StringReverse.java
+++ src/practice/StringReverse.java
@@ -1,9 +1,22 @@
 package practice;
 
+import java.util.Scanner;
+
 public class StringReverse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Reversing a string from a user input.
+		
+		System.out.println("Hey user, input a string:");
+		Scanner read = new Scanner (System.in);
+		String str = read.nextLine();
+		String reverse ="";
+		
+		for (int i =str.length()-1; i>0;i--) {
+			
+			reverse = reverse + str.charAt(i);
+		}
+		System.out.println("reversed string is "+ reverse);
 
 	}
 
