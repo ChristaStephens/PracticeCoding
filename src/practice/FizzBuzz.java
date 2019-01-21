2fb7bd37c1eb615c9c1130642407561df0a725d1
Christa Stephens <ChristaStephens@outlook.com>
2019-01-20 22:04:52 -0500

    fizzbuzz comments

--- src/practice/FizzBuzz.java
+++ src/practice/FizzBuzz.java
@@ -3,7 +3,10 @@ package practice;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Doing fizz buzz. for multiples of 3 write fizz
+		//for multiples of 5 write buzz. for multiples of 3 
+		//and 5 write fizzbuzz
+	
 
 	}
 
